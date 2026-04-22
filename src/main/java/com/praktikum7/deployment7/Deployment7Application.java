ee21370097feef4089ac7fa18a84480d5e1270bf
Andikarya17 <andikaryaputra17@gmail.com>
2026-04-22 09:46:26 +0700

    first commit

---
+++ src/main/java/com/praktikum7/deployment7/Deployment7Application.java
@@ -0,0 +1,13 @@
+package com.praktikum7.deployment7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deployment7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deployment7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum7/deployment7/Deployment7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum7.deployment7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deployment7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
